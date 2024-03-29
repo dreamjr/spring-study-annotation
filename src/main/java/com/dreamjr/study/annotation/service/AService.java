85b1c12680c5402c5db7fa4585a6e6bf9d5c8294
Chunghee Lee <dreamjr@fixelsoft.com>
2024-03-29 16:54:24 +0900

    feature-A : 파라미터 추가

--- src/main/java/com/dreamjr/study/annotation/service/AService.java
+++ src/main/java/com/dreamjr/study/annotation/service/AService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AService {
 
-    public String AFeature() {
-        return "AFeature";
+    public String AFeature(String response) {
+        return "AFeature" + response;
     }
 }

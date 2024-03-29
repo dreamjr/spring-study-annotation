5526e7212e7f3d69ed16a7aeca4c47557809bf7f
chunghee-fixel <dreamjr@fixelsoft.com>
2024-03-29 16:56:05 +0900

    feature-B : 파일 생성함

---
+++ src/main/java/com/dreamjr/study/annotation/service/BService.java
@@ -0,0 +1,11 @@
+package com.dreamjr.study.annotation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BService {
+
+    public void bFeatureMethod() {
+
+    }
+}
